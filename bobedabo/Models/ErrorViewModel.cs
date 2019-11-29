c08ab22e81e2f839aca2d028a03d05f750bfeffd
fildzanovM <53569804+fildzanovM@users.noreply.github.com>
2019-11-29 13:46:34 +0100

    Add project files.

---
+++ bobedabo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bobedabo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
